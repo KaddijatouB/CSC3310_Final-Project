d123967a213e568fa9f3a4830df8a00f0206bac0
Karena Qian <qiank@spu.edu>
2022-11-19 18:05:13 -0800

    Moved Ada, C#, & F# files into separate folders, and started adding src code into F# folder.

---
+++ F#/Unions/Program.fs
@@ -0,0 +1,19 @@
+﻿module MiiCreator
+
+let square x = x * x
+
+type Gender =
+| Male
+| Female
+
+type Hair =
+| Loose
+| Bun
+| Ponytail
+| Braid
+| Mohawk
+
+[<EntryPoint>]
+let main argv =
+    printfn "%d squared is: %d!" 12 (square 12)
+    0 // Return an integer exit code

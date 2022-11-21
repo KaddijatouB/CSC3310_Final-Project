27ca0ae57da5161d2bcae7be7df5041ab030f6eb
Kaddijatou Baldeh <baldehk@spu.edu>
2022-11-21 11:49:23 -0800

    stub implementation of record and enum

---
+++ Ada/enum_type.adb
@@ -0,0 +1,16 @@
+with Ada.Text_IO; 
+use Ada.Text_IO;
+
+procedure Main is
+-- Declay an enum for family member names
+    type MyFamily is
+    (Jerome,
+    Kay,
+    Jabin,
+    Jacobi,
+    Michaella,
+    Donald);
+    
+    begin
+
+end Main;
